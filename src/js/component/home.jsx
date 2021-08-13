098209cd6934a9a9e24289f6b7faaf8d6abce65d
Uxbel <beldetes@gmail.com>
2021-08-13 18:49:07 +0000

    Carga NavBar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+//import your own components
+import NavBar from "../component/NavBar.jsx";
+
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
@@ -7,6 +10,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	return (
 		<div className="text-center mt-5">
+			<NavBar />
 			<h1>Hello Rigoberto!</h1>
 			<p>
 				<img src={rigoImage} />
@@ -20,59 +24,6 @@ const Home = () => {
 				love!
 			</p>
 		</div>
-	);
-};
-
-const Menu = () => {
-	return (
-		<nav className="navbar navbar-expand-lg navbar-light bg-light">
-			<div className="container-fluid">
-				<a className="navbar-brand" href="#">
-					Navbar
-				</a>
-				<button
-					className="navbar-toggler"
-					type="button"
-					data-bs-toggle="collapse"
-					data-bs-target="#navbarNav"
-					aria-controls="navbarNav"
-					aria-expanded="false"
-					aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<div className="collapse navbar-collapse" id="navbarNav">
-					<ul className="navbar-nav">
-						<li className="nav-item">
-							<a
-								className="nav-link active"
-								aria-current="page"
-								href="#">
-								Home
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">
-								Features
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">
-								Pricing
-							</a>
-						</li>
-						<li className="nav-item">
-							<a
-								className="nav-link disabled"
-								href="#"
-								tabindex="-1"
-								aria-disabled="true">
-								Disabled
-							</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
 	);
 };
 
