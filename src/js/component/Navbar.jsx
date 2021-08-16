c5e3c2bc293933f0dbbb0704750d2e1db39b306a
Uxbel <beldetes@gmail.com>
2021-08-16 19:01:47 +0000

    Navbar reparado

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -3,50 +3,46 @@ import PropTypes from "prop-types";
 
 const Navbar = props => {
 	return (
-		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-			<div className="container">
-				<a className="navbar-brand" href="#">
-					{props.brand}
-				</a>
-				<button
-					className="navbar-toggler"
-					type="button"
-					data-bs-toggle="collapse"
-					data-bs-target="#navbarNav"
-					aria-controls="navbarNav"
-					aria-expanded="false"
-					aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<div
-					className="collapse navbar-collapse flex-row-reverse"
-					id="navbarNav">
-					<ul className="navbar-nav">
-						<li className="nav-item">
-							<a
-								className="nav-link active"
-								aria-current="page"
-								href="#">
-								{props.menuItemOne}
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">
-								{props.menuItemTwo}
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">
-								{props.menuItemThree}
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">
-								{props.menuItemFour}
-							</a>
-						</li>
-					</ul>
-				</div>
+		<nav className="navbar navbar-expand-lg navbar-light bg-light navbar-dark bg-dark">
+			<a className="navbar-brand" href="#">
+				{props.brand}
+			</a>
+			<button
+				className="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarNav"
+				aria-controls="navbarNav"
+				aria-expanded="false"
+				aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon"></span>
+			</button>
+			<div
+				className="collapse navbar-collapse flex-row-reverse"
+				id="navbarNav">
+				<ul className="navbar-nav">
+					<li className="nav-item active">
+						<a className="nav-link" href="#">
+							{props.menuItemOne}{" "}
+							<span className="sr-only">(current)</span>
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">
+							{props.menuItemTwo}
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">
+							{props.menuItemThree}
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">
+							{props.menuItemFour}
+						</a>
+					</li>
+				</ul>
 			</div>
 		</nav>
 	);
